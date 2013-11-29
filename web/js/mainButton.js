7c31dd7fdc5c39134424dc4ffa0f5995f928c5a0
Michał Kurcewicz <michal@getsales.pl>
2013-11-29 12:20:14 +0100

    layout: small change

--- web/js/mainButton.js
+++ web/js/mainButton.js
@@ -8,12 +8,12 @@ $(function() {
  * Set action on list main buttons
  */
 function setActionOnListMainButtons() {
-    $('#main_button > div > .refresh').click(function() {
+    $('#main_button > div > div > .refresh').click(function() {
         refreshList(true);
     });
 
-    $('#main_button > div > .edit').click(function() {
-        if (!$(this).hasClass('disable')) {
+    $('#main_button > div > div > .edit').click(function() {
+        if (!$(this).parent().hasClass('disable')) {
             var listId = List.select[0];
             var editUrl = $('#main_container > table > tbody tr[list_id="'+listId+'"]').find('.editMode').attr('href');
 
@@ -21,8 +21,8 @@ function setActionOnListMainButtons() {
         }
     });
 
-    $('#main_button > div > .remove').click(function() {
-        if (!$(this).hasClass('disable')) {
+    $('#main_button > div > div > .remove').click(function() {
+        if (!$(this).parent().hasClass('disable')) {
             deleteFromList();
         }
     });
@@ -32,17 +32,18 @@ function setActionOnListMainButtons() {
  * Set action on edit main buttons
  */
 function setActionOnEditMainButtons() {
-    $('#main_button > div > .cancel').click(function() {
+    $('#main_button > div > div > .cancel').click(function() {
         createListButtons();
         toggleListMainButton();
+        togglePagination();
         listMode();
     });
 
-    $('#main_button > div > .apply').click(function() {
+    $('#main_button > div > div > .apply').click(function() {
 
     });
 
-    $('#main_button > div > .save').click(function() {
+    $('#main_button > div > div > .save').click(function() {
         $('#edit_container > .container > form').submit();
     });
 }
@@ -115,7 +116,7 @@ function createEditButtons() {
  */
 function addMainButtons(tab) {
     $.each(tab, function(index, val) {
-         $('#main_button > .center').append(val);
+         $('#main_button > .center').append('<div class="button_container">'+val+'</div>');
     });
 }
 
@@ -123,26 +124,26 @@ function addMainButtons(tab) {
  * Enable main button edit
  */
 function enableMainButtonEdit() {
-    $('#main_button > div > .edit').removeClass('disable');
+    $('#main_button > div > div > .edit').parent().removeClass('disable');
 }
 
 /**
  * Disable main button edit
  */
 function disableMainButtonEdit() {
-    $('#main_button > div > .edit').addClass('disable');
+    $('#main_button > div > div > .edit').parent().addClass('disable');
 }
 
 /**
  * Enable main button remove
  */
 function enableMainButtonRemove() {
-    $('#main_button > div > .remove').removeClass('disable');
+    $('#main_button > div > div > .remove').parent().removeClass('disable');
 }
 
 /**
  * Disable main button remove
  */
 function disableMainButtonRemove() {
-    $('#main_button > div > .remove').addClass('disable');
+    $('#main_button > div > div > .remove').parent().addClass('disable');
 }
