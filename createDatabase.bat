8db5dde320d45f1ade7946ae3d25bebe7c2a6cce
Michał Kurcewicz <mkurc1@gmail.com>
2014-01-28 15:21:10 +0000

    createDatabase.bat deleted online with Bitbucket

--- createDatabase.bat
+++
@@ -1,5 +0,0 @@
-cd ..
-php app/console doctrine:database:drop --force
-php app/console doctrine:database:create
-php app/console doctrine:schema:update --force
-php app/console doctrine:fixtures:load --append
