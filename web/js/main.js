3e6b54d851e3594b116d73bda1e2150624a6925b
Michał Kurcewicz <mkurc1@gmail.com>
2016-02-14 10:33:43 +0100

    Remove cookie

--- web/js/main.js
+++ web/js/main.js
@@ -1,16 +1,5 @@
-$(function(){
+$(function () {
 
     $('#main_menu ul li.has_submenu > a').removeAttr('href');
-
-    //cookies policy
-    if (getCookie('cookies_policy') != "true") {
-        $('#cookie').show();
-    }
-
-    $('#cookie').find('a.close').click(function() {
-        $('#cookie').remove();
-
-        setCookie('cookies_policy', true, 365);
-    });
 
 });
